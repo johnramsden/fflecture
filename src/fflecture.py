6a7e248cdacf9595ad0a7bb2bd51da56a1ed81ab
John Ramsden <(none)>
2016-09-11 16:54:11 -0700

    Setup lecture class

--- src/fflecture.py
+++ src/fflecture.py
@@ -4,25 +4,49 @@ import subprocess as sp
 import argparse
 
 
-class Editor:
-    FFMPEG_BIN = "ffmpeg"
+class Lecture:
+    def __init__(self, titles: list, background, sequence: list):
+        self.__titles = titles
+        self.__background = background
+        self.__sequence = sequence
 
+
+
+class Editor:
+    commands = [[__FFMPEG_BIN, '-h']]
+
+    def set_ffmpeg_bin(self, ffmpeg_bin=None):
+        # Default to linux binary
+        if ffmpeg_bin is None:
+            self.__FFMPEG_BIN = "ffmpeg"
+        else:
+            self.__FFMPEG_BIN = ffmpeg_bin
+
+    def editor_info(self):
+        print("## Editor information ##")
+        print("Binary: ", self.__FFMPEG_BIN)
+
+    def create_intro(self):
+
+    def create_video_sequence(self):
+
+    def setup_commands(self):
+
+
+    def run(self):
+        for cmd in self.commands:
+            sp.call(cmd)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Create lecture with ffmpeg.')
-    parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                        help='an integer for the accumulator')
-    parser.add_argument('--sum', dest='accumulate', action='store_const',
-                        const=sum, default=max,
-                        help='sum the integers (default: find the max)')
-
     args = parser.parse_args()
-    print(args.accumulate(args.integers))
 
 
 def main():
     parse_arguments()
-
+    video_editor = Editor()
+    video_editor.editor_info()
+    video_editor.run()
 
 if __name__ == "__main__":
     main()
