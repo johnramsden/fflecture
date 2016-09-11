f096b4b556ed4a8e807b8e9499097597d7c39965
John Ramsden <(none)>
2016-09-11 15:30:17 -0700

    Added argument parser

--- src/fflecture.py
+++ src/fflecture.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python
 
+import subprocess as sp
 import argparse
 
 
+class Editor:
+    FFMPEG_BIN = "ffmpeg"
 
-def main()
 
-if __name__ == "__main__"
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Create lecture with ffmpeg.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                        help='an integer for the accumulator')
+    parser.add_argument('--sum', dest='accumulate', action='store_const',
+                        const=sum, default=max,
+                        help='sum the integers (default: find the max)')
+
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
+
+
+def main():
+    parse_arguments()
+
+
+if __name__ == "__main__":
     main()
