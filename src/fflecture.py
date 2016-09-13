f0f8a7087e4c2ed891632f614bbcc9d315f9b718
John Ramsden <(none)>
2016-09-13 01:04:21 -0700

    Added filter to concat videos

--- src/fflecture.py
+++ src/fflecture.py
@@ -2,7 +2,7 @@
 
 import subprocess as sp
 import argparse
-
+import os.path
 
 class Lecture:
     def __init__(self, lecture_title, class_title,
@@ -30,28 +30,45 @@ class Editor:
         print("Binary: ", self.ffmpeg)
 
     # Returns ffmpeg command in list to create video from image
-    def create_background_video(self, background, length):
-        return  ['-loop', '1',
-                 '-i', background,
-                 '-c:v', 'libx264',
-                 '-t', str(length),
+    def background_video_command(self, background, length):
+        return  [self.ffmpeg, '-loop', '1', '-i', background,
+                 '-c:v', 'libx264', '-t', str(length),
                  '-pix_fmt', 'yuvj444p',
-                 'background.mp4']
+                 '-f', 'mpegts', 'background.ts']
 
-    def create_intro_filter(self):
+    #ffmpeg -i intermediate1.ts -filter_complex
+    # "drawtext=enable='between(t,1,9)':
+    # fontfile=/usr/share/fonts/TTF/WorkSans-Bold.ttf:
+    # text='STAT302': fontcolor=white: fontsize=148:
+    # box=1: boxcolor=black@0.5: boxborderw=25: x=(w-text_w)/2: y=(h-text_h)/2 [v];
+    # [v] drawtext=enable='between(t,11,19)':
+    # fontfile=/usr/share/fonts/TTF/WorkSans-Bold.ttf:
+    # text='Lecture 01': fontcolor=white: fontsize=148:
+    # box=1: boxcolor=black@0.5: boxborderw=25: x=(w-text_w)/2: y=(h-text_h)/2"
+    # -codec:a copy output.mp4
+    def create_intro_titles(self, course_title, lecture_title, font, fontsize, title_start, title_end):
+        filter = [drawtext=enable='between(t,,)']
 
-    # ffmpeg -loop 1 -i stats-background.jpg -i MATH220-E01-part1.MTS -i MATH220-E01-part2.MTS -filter_complex "[0:v:0]duration=10[begin];[begin]drawtext=enable='between(t,1,5)':fontfile=/usr/share/fonts/truetype/freefont/FreeSerif.ttf:text='MATH220'[halfintro];drawtext=enable='between(t,6,10)':fontfile=/usr/share/fonts/truetype/freefont/FreeSerif.ttf:text='Lecture 01'[intro];[intro][1:v:0][1:a:0][2:v:0][2:a:0]concat=n=3:v=1:a=1[v][a]" -map "[v]" -map "[a]" o.mp4
-
+    # ffmpeg -loop 1 -i stats-background.jpg -i MATH220-E01-part1.MTS -i MATH220-E01-part2.MTS -filter_complex
+    # "[0:v:0]duration=10[begin];[begin]drawtext=enable='between(t,1,5)':fontfile=/usr/share/fonts/truetype/freefont/FreeSerif.ttf:text='MATH220'[halfintro];drawtext=enable='between(t,6,10)':fontfile=/usr/share/fonts/truetype/freefont/FreeSerif.ttf:text='Lecture 01'[intro];[intro][1:v:0][1:a:0][2:v:0][2:a:0]concat=n=3:v=1:a=1[v][a]" -map "[v]" -map "[a]" o.mp4
 
     # FFMPEG Command:
     # ffmpeg -i MATH220-E01-part1.MTS -i MATH220-E01-part2.MTS -filter_complex
     # "[0:v:0] [0:a:0] [1:v:0] [1:a:0] concat=n=2:v=1:a=1 [v] [a]"
     # -map "[v]" -map "[a]" o.mp4
-    def video_filter(self, number_video):
-        return '"[0:v:0] [0:a:0] [1:v:0] [1:a:0] concat=n={}:v=1:a=1 [v] [a]"'.format(number_video)
+    def Concat_video_filter(self, video_quantity):
+        concat_filter = []
+        for video in range(video_quantity):
+            concat_filter.extend(["[{0}:v:0]".format(video),
+                           "[{}:a:0]".format(video)])
+            concat_filter.extend(["concat=n={}:v=1:a=1".format(video_quantity), "[v]", "[a]"])
+        return concat_filter
 
-    # def setup_commands(self):
-
+    def Concat_video_command(self, input_videos: list):
+        video_command = [self.ffmpeg]
+        for video in input_videos.len():
+            video_command.extend(['-i', video])
+        return video_command
 
     def run(self):
         for cmd in self.commands:
