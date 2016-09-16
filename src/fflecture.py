37e5741406aaff5f35efbd615853386d3d5c7e0e
John Ramsden <(none)>
2016-09-15 19:26:06 -0700

    Finished intro_title_command

--- src/fflecture.py
+++ src/fflecture.py
@@ -4,39 +4,66 @@ import subprocess as sp
 import argparse
 import os.path
 
+
 class Lecture:
     def __init__(self, lecture_title, class_title,
-                 background, sequence: list, intro_length: int):
+                 background, video_sequence: list):
         self.lecture_title = lecture_title
         self.class_title = class_title
         self.background = background
-        self.sequence = sequence
-        self.intro_length = intro_length
+        self.video_sequence = video_sequence
 
     def describe_lecture(self):
         print(self.class_title, " ", self.lecture_title)
-        print("Using intro background ", self.background,
-              " for ", self.intro_length, "seconds.")
-        print("Video sequence: ", *self.sequence)
+        print("Using intro background ", self.background)
+        print("Video sequence: ", *self.video_sequence)
 
 
 class Editor:
-    def __init__(self, ffmpeg):
+    def __init__(self,
+                 ffmpeg: str="ffmpeg",
+                 font_file=None,
+                 font_size: int=148,
+                 font_color: str="white",
+                 title_length: int=5,
+                 title_x: str="(w-text_w)/2",
+                 title_y: str="(h-text_h)/2",
+                 box: int=1,
+                 box_color: str="black",
+                 box_opacity: int=0.5,
+                 box_border_width: int=25):
         self.ffmpeg = ffmpeg
-        self.commands = [[self.ffmpeg, '-h']]
+        self.commands = {'help': [self.ffmpeg, '-h']}
+        self.command_sequence = []
+        self.titles= {"font": {"file": font_file,
+                               "size": font_size,
+                               "color": font_color},
+                      "length": title_length,
+                      "box": {"status": box,
+                              "color": box_color,
+                              "opacity": box_opacity,
+                              "borderw": box_border_width},
+                      "coordinates": {"x": title_x,
+                                      "y": title_y}}
 
     def editor_info(self):
         print("## Editor information ##")
         print("Binary: ", self.ffmpeg)
 
-    # Returns ffmpeg command in list to create video from image
-    def background_video_command(self, background, length):
-        return  [self.ffmpeg, '-loop', '1', '-i', background,
-                 '-c:v', 'libx264', '-t', str(length),
-                 '-pix_fmt', 'yuvj444p',
-                 '-f', 'mpegts', 'background.ts']
+    def image_video_command(self, background: str, output_stream: str="temp.ts"):
+        """
+        ffmpeg command in list to create video from image
+        :param background:
+        :param output_stream:
+        :return:
+        """
+        return {"command": [self.ffmpeg, '-loop', '1', '-i', background,
+                            '-c:v', 'libx264', '-t', self.titles['length'],
+                            '-pix_fmt', 'yuvj444p',
+                            '-f', 'mpegts', output_stream],
+                "stream": output_stream}
 
-    #ffmpeg -i intermediate1.ts -filter_complex
+    # ffmpeg -i intermediate1.ts -filter_complex
     # "drawtext=enable='between(t,1,9)':
     # fontfile=/usr/share/fonts/TTF/WorkSans-Bold.ttf:
     # text='STAT302': fontcolor=white: fontsize=148:
@@ -46,38 +73,72 @@ class Editor:
     # text='Lecture 01': fontcolor=white: fontsize=148:
     # box=1: boxcolor=black@0.5: boxborderw=25: x=(w-text_w)/2: y=(h-text_h)/2"
     # -codec:a copy output.mp4
-    # def create_intro_titles(self, course_title, lecture_title, font, fontsize, title_start, title_end):
-    #     intro_filter = [drawtext=enable='between(t,,)']
+
+    def intro_titles_command(self, background: str, titles: list, output_video: str):
+        """
+        drawtext=enable='between(t,1,9)':
+        fontfile=/usr/share/fonts/TTF/WorkSans-Bold.ttf:
+        text='STAT302': fontcolor=white: fontsize=148:
+        box=1: boxcolor=black@0.5: boxborderw=25: x=(w-text_w)/2: y=(h-text_h)/2 [v]
+        """
+        image_video = self.image_video_command(background)
+        intro_titles_command = image_video['command']
+
+        # Setup filter for intro titles
+        intro_titles_filter = []
+        for index in range(len(titles)):
+            # Setup interval text will display for
+            intro_titles_filter.extend(["drawtext=enable='between(t,{0},{1})'"
+                                       .format(str(index * self.titles['length'] + 1),
+                                               str((index + 1) * self.titles['length'] - 1)),
+                                        "fontfile={}".format(self.titles['font']['file']),
+                                        "fontcolor={}".format(self.titles['font']['color']),
+                                        "fontsize={}".format(str(self.titles['font']['size'])),
+                                        "box={}".format(str(self.titles['box']['status'])),
+                                        "boxcolor={0}@{1}".format(self.titles['box']['color'],
+                                                                  str(self.titles['box']['opacity'])),
+                                        "boxborderw={}".format(str(self.titles['box']['borderw'])),
+                                        "x={}".format(self.titles['coordinates']['x']),
+                                        "y={}".format(self.titles['coordinates']['y'])])
+        return intro_titles_command + ['&&', self.ffmpeg, '-i', image_video['stream'],
+                                       '-filter_complex', '"{}"'.format(":".join(intro_titles_filter)),
+                                       '-codec:a', 'copy', output_video]
 
     # ffmpeg -loop 1 -i stats-background.jpg -i MATH220-E01-part1.MTS -i MATH220-E01-part2.MTS -filter_complex
     # "[0:v:0]duration=10[begin];[begin]drawtext=enable='between(t,1,5)':fontfile=/usr/share/fonts/truetype/freefont/FreeSerif.ttf:text='MATH220'[halfintro];drawtext=enable='between(t,6,10)':fontfile=/usr/share/fonts/truetype/freefont/FreeSerif.ttf:text='Lecture 01'[intro];[intro][1:v:0][1:a:0][2:v:0][2:a:0]concat=n=3:v=1:a=1[v][a]" -map "[v]" -map "[a]" o.mp4
 
-    # FFMPEG Command:
-    # ffmpeg -i MATH220-E01-part1.MTS -i MATH220-E01-part2.MTS -filter_complex
-    # "[0:v:0] [0:a:0] [1:v:0] [1:a:0] concat=n=2:v=1:a=1 [v] [a]"
-    # -map "[v]" -map "[a]" o.mp4
-    def Concat_video_filter(self, video_quantity):
-        concat_filter = []
-        for video in range(video_quantity):
-            concat_filter.extend(["[{0}:v:0]".format(video),
-                           "[{}:a:0]".format(video)])
-        concat_filter.extend(["concat=n={}:v=1:a=1".format(video_quantity), "[v]", "[a]"])
-        return concat_filter
-
-    def Concat_video_command(self, input_videos: list):
+    def concat_video_command(self, videos: list, concat_video: str):
+        """
+        Setup the ffmpeg command used to concatenate videos.
+        :return:
+        """
         video_command = [self.ffmpeg]
-        for video in len(input_videos):
+        for video in videos:
             video_command.extend(['-i', video])
+
+        """
+        Setup the filter to concatenate videos. e.g. With n=2:
+        "[0:v:0] [0:a:0] [1:v:0] [1:a:0] concat=n=2:v=1:a=1 [v] [a]"
+        """
+        concat_filter = []
+        for video in range(len(videos)):
+            concat_filter.extend(["[{0}:v:0]".format(video), "[{}:a:0]".format(video)])
+        concat_filter.extend(["concat=n={}:v=1:a=1".format(len(videos)), "[v]", "[a]"])
+
+        # Add the filter items in a string, surrounded by quotes
+        video_command.extend(['"{}"'.format(" ".join(concat_filter)),
+                              '-map', '"[v]"', '-map', '"[a]"', concat_video])
         return video_command
 
     def run(self):
         for cmd in self.commands:
             sp.call(cmd)
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Create lecture with ffmpeg.')
     parser.add_argument('-b', '--binary',
-                        help='Specify ffmpeg binary, default is ffmpeg')
+                        help='Specify ffmpeg binary, default is "ffmpeg"')
     parser.add_argument('-v', '--video', action='append',
                         help='Add one or more video.')
     parser.add_argument('-c', '--coursetitle', default='Course',
@@ -94,23 +155,30 @@ def parse_arguments():
     parser.add_argument('-n', help="Run in test mode, will not output anything.")
     return parser.parse_args()
 
+
 def main():
     # Get commandline input
     arguments = parse_arguments()
 
     # Create Lecture
     lecture = Lecture(arguments.lecturetitle, arguments.coursetitle,
-                      arguments.picture, arguments.video, arguments.titlelength)
+                      arguments.picture, arguments.video)
     lecture.describe_lecture()
 
     # Create video editor
-    video_editor = Editor('ffmpeg')
-    print(video_editor.Concat_video_filter(len(arguments.video)))
+    video_editor = Editor('ffmpeg', font_file=arguments.font, title_length=int(arguments.titlelength))
+    # print(video_editor.concat_video_command(lecture.video_sequence))
+    # print(video_editor.image_video_command(arguments.picture, arguments.titlelength))
 
-    # background_cmd = [video_editor.ffmpeg] + video_editor.create_background_video(
-    #     '/home/john/Workspace/fflecture/resources/stats-background.jpg', 30)
+    title_video = 'intro_title.mp4'
+
+    for item in video_editor.intro_titles_command(
+            lecture.background,[lecture.class_title, lecture.lecture_title], title_video):
+        print(item)
+
     # video_editor.commands.append(background_cmd)
-    #video_editor.run()
+    # video_editor.run()
+
 
 if __name__ == "__main__":
     main()
